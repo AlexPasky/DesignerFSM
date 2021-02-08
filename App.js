e129600277c71727d785baa23fe14b6f17b0804c
AlexPasky <paskyab@gmail.com>
2021-02-08 12:43:46 +0300

    added new file

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Product from './Product';
+import User from './User';
+
+function App() {
+	return <div>
+		<Product />
+		<User />
+		text
+	</div>;
+}
+
+export default App;
